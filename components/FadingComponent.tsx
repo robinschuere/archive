0546177a94b9973d2fcc2e4182e408106cf97183
Robin Schuerewegen <robke_schuere@hotmail.com>
2024-10-23 14:15:42 +0200

    Create FadingComponent.tsx

---
+++ components/FadingComponent.tsx
@@ -0,0 +1,36 @@
+import { useState, useEffect, PropsWithChildren } from 'react';
+
+interface FadingComponentProps {
+  /* Should the component be visible */
+  visible: boolean;
+  /* Defines how long the component should be visible in ms */
+  time: number;
+  /* The event that bubbles whenever the component will stop being showed */
+  onChange?: () => void;
+}
+
+const FadingComponent = (Component: JSX.Element) => ({ visible, time, onChange, children }: PropsWithChildren<FadingComponentProps>) => {
+  const [showComponent, setShowComponent] = useState(false);
+
+  useEffect(() => {
+    setShowComponent(visible)
+    let timeoutId = null;
+    if (visible) {
+      timeoutId = setTimeout(() => {
+        setShowComponent(false)
+        if (onChange) {
+          onChange();
+        }
+      }, time);
+    }
+    return () => {
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
+    }
+  },[visible, time]);
+
+  return show ? children : null;
+}
+
+export default VanishingComponent;
