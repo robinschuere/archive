fb6aa90fb64e3decb007a583ed37e310e91bd83f
Robin Schuerewegen <robke_schuere@hotmail.com>
2024-11-13 11:47:48 +0100

    Update types.d.ts

--- context/OfflineContext/types.d.ts
+++ context/OfflineContext/types.d.ts
@@ -1,3 +1,19 @@
+type BaseEntity = {
+  id: string;
+  updatedAt?: number;
+  removedAt?: number;
+}
+
+export type ActionContext<T extends BaseEntity> = {
+  [key: string]: {
+    get: (id: string) => Promise<T>;
+    update: (id: string, params: any[]) => Promise<void>;
+    remove: (id: string) => Promise<void>;
+    insert: (id: string, params: any[]) => Promise<void>;
+    check: (params: any[]) => Promise<boolean>;
+  }
+}
+
 export type OfflineValue = {
   contextValue: string;
   contextAction: 'update' | 'remove' | 'insert';
