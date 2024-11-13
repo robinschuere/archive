9d84ef20f5fe75d42f5c2c34092bc66c4e4e4206
Robin Schuerewegen <robke_schuere@hotmail.com>
2024-11-13 13:12:41 +0100

    Update actions.ts

--- context/OfflineContext/actions.ts
+++ context/OfflineContext/actions.ts
@@ -1,11 +1,14 @@
 import { ActionContext } from './types';
 
-export const actions = {
-  todo: {
+// this is a mockExample since api does not exist here!
+const todoActions: ActionContext<Todo> = {
     get: async (id: string) => api.getTodo(id),
     update: async (id: string, params: any[]) => api.updateTodo(id, ...params),
     remove: async (id: string) => api.removeTodo(id),
     insert: async (params) => api.addTodo(...params),
     check: async (params) => api.checkIfDuplicateTodo(...params),
-  } as ActionContext<Todo>
+  }
+
+export const actions = {
+  todo: todoActions
 }
