4d87e3caa3e30d622db308960083afe95c8f1cfb
Robin Schuerewegen <robke_schuere@hotmail.com>
2024-11-13 13:32:26 +0100

    Update syncer.ts

--- context/OfflineContext/syncer.ts
+++ context/OfflineContext/syncer.ts
@@ -1,14 +1,20 @@
 export const syncer = (actions) => async (value: OfflineValue) => {
   const context = actions[value.contextValue];
-  if(!context) {
-    console.error(`Context is available for value '${value.contextValue}'. See to it that the functions 'get', 'insert', 'update', 'remove' and 'check' are available as wel as the context!`);
+  
+  if (!context) {
+    console.error(`Not existing context was given: '${value.contextValue}'. Make sure the provider gets an actions object that holds contexts and their actions.`);
     return false;
   }
-  if(!context.get || !context.insert || !context.update || !context.remove || !context.check) {
-    console.error(`Context is not complete for '${value.contextValue}'. See to it that the functions 'get', 'insert', 'update', 'remove' and 'check' are available!`);
+  if (!context.get || !context.insert || !context.update || !context.remove || !context.check) {
+    console.error(`Context '${value.contextValue}' is not complete. See to it that the functions 'get', 'insert', 'update', 'remove' and 'check' are always available!`);
     return false;
   }
-  if(value.contextAction === 'insert') {
+  if (!['insert', 'update', 'remove'].includes(value.contextAction)) {
+    console.error(`the given action '${value.contextAction}' is not able to be resolved! Make sure that the dispatch action holds the correct data values!`);
+    return false;
+  }
+  
+  if (value.contextAction === 'insert') {
     const isDuplicate = await context.check(value.params);
     if (!isDuplicate) {
       await context.insert(value.params);
