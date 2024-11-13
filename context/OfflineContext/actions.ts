6d1a9a714943bcf0a5d0be1e0da0cab771ac072b
Robin Schuerewegen <robke_schuere@hotmail.com>
2024-11-13 13:39:16 +0100

    Update actions.ts

--- context/OfflineContext/actions.ts
+++ context/OfflineContext/actions.ts
@@ -6,7 +6,9 @@ const todoActions: ActionContext<Todo> = {
     update: async (id: string, params: any[]) => api.updateTodo(id, ...params),
     remove: async (id: string) => api.removeTodo(id),
     insert: async (params) => api.addTodo(...params),
-    check: async (params) => api.checkIfDuplicateTodo(...params),
+    checkInsert: async (params) => api.checkInsertTodo(...params),
+    checkUpdate: async (params) => api.checkUpdateTodo(...params),
+    checkRemove: async (params) => api.checkRemoveTodo(...params),
   }
 
 export const actions = {
