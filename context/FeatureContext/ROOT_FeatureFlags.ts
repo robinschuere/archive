4c5848e1b86ff6088c7c114866ecafd682d213cd
Robin Schuerewegen <robke_schuere@hotmail.com>
2024-12-09 16:56:01 +0100

    Update ROOT_FeatureFlags.ts

--- context/FeatureContext/ROOT_FeatureFlags.ts
+++ context/FeatureContext/ROOT_FeatureFlags.ts
@@ -1,13 +1,21 @@
 export type FeatureFlag = {
-  [key: string]: {
-    name: string;
-    description: string;
-  }
+  [key: string]: string
 }
 
 const FeatureFlags: FeatureFlag = {
-  FEATUREA_
-
-export const getFeatures = (featureFlagNames: string[]) => {
-
+  FEATURE_A_WORKS_AS_INTENDED: 'The feature works with all steps included',
+  FEATURE_B_STEP_1: 'Step 1 of the feature is enabled',
+  FEATURE_B_STEP_2: 'Step 2 of the feature is enabled',
+  FEATURE_B_WORKS_AS_INTENDED: 'The feature works with all steps included',
 }
+
+const enabledFeatures = (featureFlagNames: string[]) => {
+  return featureFlagNames.map(name => FeatureFlag[name]).filter(boolean);
+}
+
+export const hasFeature = (featureFlagNames: string[]) => {
+  const features = enabledFeatures(featureFlagNames);
+  return (featureName: string) => {
+    return features[featureName];
+  };
+};
