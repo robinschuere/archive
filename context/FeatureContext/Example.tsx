f9ac4afda83491803ffee0b3a059cb534fd0c75d
Robin Schuerewegen <robke_schuere@hotmail.com>
2024-12-09 16:59:17 +0100

    Update Example.tsx

--- context/FeatureContext/Example.tsx
+++ context/FeatureContext/Example.tsx
@@ -3,6 +3,17 @@ import { FeatureA, FeatureB } from '@features';
 
 import features from './FeatureFlags';
 
+// code sample from a feature (Mind that the feature knows of the context!)
+const FeatureC = () => {
+  const context = useFeatureContext();
+
+  if (context.hasFeature('FEATURE_C_WORKS_AS_INTENDED')) {
+    return <ACOMPLEXCOMPONENT />
+  }
+  return null;
+};
+
+
 const Example = () => {
   return <FeatureProvider features={features}>
     <FeatureA />
