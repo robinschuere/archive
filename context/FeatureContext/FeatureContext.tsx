e992b1c01d28cec6b315ac1c403b39e772b74edb
Robin Schuerewegen <robke_schuere@hotmail.com>
2024-12-09 16:36:22 +0100

    Update FeatureContext.tsx

--- context/FeatureContext/FeatureContext.tsx
+++ context/FeatureContext/FeatureContext.tsx
@@ -1,1 +1,15 @@
-test
+import { createContext } from 'react';
+
+const FeatureContext = createContext(null);
+
+export const FeatureProvider = ({ features }) => {  
+  return (
+    <FeatureContext.Provider value={{ features }}>
+      {children}
+    </FeatureContext.Provider>
+  );
+}
+
+export const useFeatureContext = () => {
+  return useContext(FeatureContext)
+}
