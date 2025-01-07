cad585db1c4e842a9ad7b78000e1f275b58e2966
Robin Schuerewegen <robke_schuere@hotmail.com>
2025-01-07 16:23:18 +0100

    Create constants.ts

---
+++ components/HolidayCalendar/constants.ts
@@ -0,0 +1,33 @@
+export const months = [
+  'januari',
+  'februari',
+  'march',
+  'april',
+  'may',
+  'june',
+  'july',
+  'august',
+  'september',
+  'october',
+  'november',
+  'december',
+];
+
+export const holidays = [
+  '1|0',
+  '21|3',
+  '1|4',
+  '29|4',
+  '9|5',
+  '21|6',
+  '15|7',
+  '1|10',
+  '11|10',
+  '25|11',
+];
+
+export const customStyle = {
+  width: '45px',
+  height: '45px',
+  border: '1px solid',
+};
