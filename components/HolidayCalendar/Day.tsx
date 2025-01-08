8453dc1f6cebf5c3dfa638f7981e0f5450ca32cc
Robin Schuerewegen <robke_schuere@hotmail.com>
2025-01-08 10:11:21 +0100

    Update Day.tsx

--- components/HolidayCalendar/Day.tsx
+++ components/HolidayCalendar/Day.tsx
@@ -2,32 +2,31 @@ import { useState } from 'react';
 import { customStyle } from './constants';
 
 const Day = ({
+    active,
     year,
     day,
     month,
-    addDay,
-    removeDay,
-    dayNoWork,
+    toggleDay,
+    daysOfNoWork,
   }: {
+    active: boolean;
     year: number;
     day: number;
     month: number;
-    addDay: () => void;
-    removeDay: () => void;
-    dayNoWork: number;
+    toggleDay: (dayMonth: string) => void;
+    daysOfNoWork: number[];
   }) => {
     const date = new Date(year, month, day, 12);
-    const [active, setActive] = useState(false);
 
+    // Since we drop dates like the 31 of februari, we will not draw anything when we surpass the given month. Neat XD
     if (date.getMonth() !== month) return <div />;
 
-    if (date.getDay() === dayNoWork) {
-      return <Holiday color="pink" />;
-    }
-
-    if (date.getDay() === 0 || date.getDay() === 6) {
+    // Not every day is a workday. We can drop the weekends and extra days
+    if (daysOfNoWork.includes(date.getDay()) {
       return <Holiday color="gray" />;
     }
+
+    // Official holidays are also absent.
     if (
       holidays.find((dayMonth) => {
         return dayMonth === `${day}|${month}`;
@@ -35,20 +34,14 @@ const Day = ({
     ) {
       return <Holiday color="red" />;
     }
+    
     return (
       <div
         style={{
           ...customStyle,
           'background-color': active ? 'green' : 'white',
         }}
-        onClick={() => {
-          setActive(!active);
-          if (active) {
-            removeDay();
-          } else {
-            addDay();
-          }
-        }}
+        onClick={() => toggleDay(`${day}|${month}`)}
       ></div>
     );
   };
