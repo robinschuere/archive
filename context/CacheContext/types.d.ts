95a70b0599647eda81c0f6d32b154601d1dd882c
Robin Schuerewegen <robke_schuere@hotmail.com>
2025-03-14 12:57:01 +0100

    Update types.d.ts

--- context/CacheContext/types.d.ts
+++ context/CacheContext/types.d.ts
@@ -1,12 +1,14 @@
-type CacheStratgy = {
-  type: 'refresh';
-  timeInMs: number;
-}
-
 type DataConfig = {
   strategy: CacheStrategy;
   get: () => Promise<any>;
   onError: () => void;
+  cacheTimeInMs: number;
+}
+
+type DataValue = DataConfig & {
+  refreshAfter: number;
 }
 
 export type CachedDataConfig = Record<string, DataConfig>
+
+export type CachedData = Record<string, DataValue>
