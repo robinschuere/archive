daecad375ae83e31266284d935b49c9a3dd4f3e9
Robin Schuerewegen <robke_schuere@hotmail.com>
2025-03-14 13:39:34 +0100

    Update CacheContext.tsx

--- context/CacheContext/CacheContext.tsx
+++ context/CacheContext/CacheContext.tsx
@@ -1,10 +1,16 @@
-import { createContext } from 'react';
+import { createContext, useEffect } from 'react';
+import { initialize, checkData, getData, refreshData } from './cache';
 
 const CacheContext = createContext(null);
 
 const CacheProvider = ({ dataConfig }) => {
+  useEffect(() => {
+    initialize();
+    setInterval(checkData, 60 * 1000);
+  }, []);
+  
   return (
-    <CacheContext.Provider value={{  }}>
+    <CacheContext.Provider value={{ getData, refreshData }}>
       {children}
     </CacheContext.Provider>
   );
