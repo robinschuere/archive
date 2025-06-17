fe97f3d9ea09f666ee0a3316253fd089515a043e
Robin Schuerewegen <robke_schuere@hotmail.com>
2025-06-17 12:30:57 +0200

    Update HorizontalScrollExample.tsx

--- components/HorizontalScrollExample.tsx
+++ components/HorizontalScrollExample.tsx
@@ -3,7 +3,7 @@ export default function MainPage () {
     <div style={{ height: "100vh", width: "100vw", overflow: 'hidden'}}>
       <div style={{ display: 'flex',  flexDirection: 'column', height: '100%' }}>
         <div style={{ backgroundColor:'red' }}>The header div (Not scrollable)</div>
-        <div style={{ backgroundColor:'blue', flex-grow: '1 1 auto', minHeight: 0, overflowY: 'auto' }}>
+        <div style={{ backgroundColor:'blue', flex-grow: 1, minHeight: 0, overflowY: 'auto' }}>
           <div>TEST</div>
           <div>TEST</div>
           <div>TEST</div>
