cedec44008ab4c186afe28b8361665a39d854ab1
Robin Schuerewegen <robke_schuere@hotmail.com>
2025-06-17 12:29:55 +0200

    Create HorizontalScrollExample.tsx

---
+++ components/HorizontalScrollExample.tsx
@@ -0,0 +1,43 @@
+export default function MainPage () {
+  return (
+    <div style={{ height: "100vh", width: "100vw", overflow: 'hidden'}}>
+      <div style={{ display: 'flex',  flexDirection: 'column', height: '100%' }}>
+        <div style={{ backgroundColor:'red' }}>The header div (Not scrollable)</div>
+        <div style={{ backgroundColor:'blue', flex-grow: '1 1 auto', minHeight: 0, overflowY: 'auto' }}>
+          <div>TEST</div>
+          <div>TEST</div>
+          <div>TEST</div>
+          <div>TEST</div>
+          <div>TEST</div>
+          <div>TEST</div>
+          <div>TEST</div>
+          <div>TEST</div>
+          <div>TEST</div>
+          <div>TEST</div>
+          <div>TEST</div>
+          <div>TEST</div>
+          <div>TEST</div>
+          <div>TEST</div>
+          <div>TEST</div>
+          <div>TEST</div>
+          <div>TEST</div>
+          <div>TEST</div>
+          <div>TEST</div>
+          <div>TEST</div>
+          <div>TEST</div>
+          <div>TEST</div>
+          <div>TEST</div>
+          <div>TEST</div>
+          <div>TEST</div>
+          <div>TEST</div>
+          <div>TEST</div>
+          <div>TEST</div>
+          <div>TEST</div>
+          <div>TEST</div>
+          <div>TEST</div>
+          <div>TEST</div>
+        </div>
+      </div>
+    </div>
+  );
+}
